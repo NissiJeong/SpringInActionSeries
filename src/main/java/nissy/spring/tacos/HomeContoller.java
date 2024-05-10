e99d552d48a1aa239cd8bf78c66debc254a49784
NissiJeong <jnissi92@gmail.com>
2024-05-10 17:02:18 +0900

    first commit

---
+++ src/main/java/nissy/spring/tacos/HomeContoller.java
@@ -0,0 +1,15 @@
+package nissy.spring.tacos;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeContoller {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+    
+}
